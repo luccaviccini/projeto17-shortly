3c169384fb02955f767cdbd0f2a83e99c7481bbb
luccaviccini <luccaviccini@gmail.com>
2023-03-02 12:55:12 -0300

    feat: rename files and funtion

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { signUpSchema } from "../schemas/user.schema.js";
-import { loginSchema } from "../schemas/login.schema.js";
+import { signInSchema } from "../schemas/signIn.schema.js";
 import { signUp, signIn } from "../controllers/users.controller.js";
 
 const router = Router();
 
 router.post("/signup",validateSchema(signUpSchema), signUp);
-router.post("/signin", validateSchema(loginSchema), signIn);
+router.post("/signin", validateSchema(signInSchema), signIn);
 
 export default router;
