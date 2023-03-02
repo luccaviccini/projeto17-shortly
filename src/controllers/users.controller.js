de6639be992867c2b2855c63dd5e1fcfc0e32be0
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-03-02 19:33:26 -0300

    feat: users/me ok

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,15 +2,12 @@ import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
 
-
-
 export async function userSignUp(req, res) {
   const { name, email, password } = req.body;
 
-  const hashedPassword = bcrypt.hashSync(password, 10);   
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
-  try {  
-
+  try {
     // create user
     await db.query(
       ` INSERT INTO users 
@@ -18,9 +15,8 @@ export async function userSignUp(req, res) {
                     VALUES ($1, $2, $3)`,
       [name, email, hashedPassword]
     );
-    
+
     res.status(201).send("User created in database with success.");
-    
   } catch (err) {
     return res.status(500).send("Erro no signUp" + err);
   }
@@ -42,7 +38,6 @@ export async function userSignIn(req, res) {
 
     // compare passwords
     if (bcrypt.compareSync(password, user.password)) {
-      
       const token = uuid();
       await db.query(`INSERT INTO sessions (token, "userId") VALUES ($1, $2)`, [
         token,
@@ -51,12 +46,10 @@ export async function userSignIn(req, res) {
 
       res.locals.session = {
         userId: user.id,
-        token
+        token,
       };
       return res.status(200).send({ token });
     }
-
-    
 
     res.sendStatus(401);
   } catch (err) {
@@ -64,3 +57,29 @@ export async function userSignIn(req, res) {
   }
 }
 
+export async function getUsers(req, res) {
+  const { userId } = res.locals.session;
+  try {
+    const queryResult = await db.query(
+      `SELECT users.id, users.name, 
+       (SELECT SUM("visitCount") FROM urls WHERE "userId" = users.id) AS "visitCount", 
+       (SELECT json_agg(json_build_object('id', urls.id, 'shortUrl', urls."shortUrl", 'url', urls.url, 'visitCount', urls."visitCount"))
+        FROM urls WHERE "userId" = users.id) AS "shortenedUrls"
+        FROM users
+        WHERE users.id = $1;
+        `,
+      [userId]
+    );
+
+    const urlData = queryResult.rows[0];
+
+    if (!urlData) {
+      return res.status(404).send("Url not found");
+    }
+
+    res.status(200).send(urlData);
+
+  } catch (err) {
+    return res.status(500).send("Erro no getUsers" + err.message);
+  }
+}
