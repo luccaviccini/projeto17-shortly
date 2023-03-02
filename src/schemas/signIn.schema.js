80d3a82cb62c6164597ca4e7cbff06cf02a90d18
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-03-02 18:59:03 -0300

    fix: putting pass min the same as sign up

--- src/schemas/signIn.schema.js
+++ src/schemas/signIn.schema.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 export const signInSchema = joi.object({
   email: joi.string().email().required(),
-  password: joi.string().min(6).required(),
+  password: joi.string().min(3).required(),
 });
 
 
