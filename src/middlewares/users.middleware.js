954d670d13cf84dbbf4ca66d4d1ef7c5c62e0c92
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-03-02 15:15:21 -0300

    refactor: removing bcrypt from middleware

--- src/middlewares/users.middleware.js
+++ src/middlewares/users.middleware.js
@@ -3,7 +3,9 @@ import {db} from "../database/database.connection.js";
 
 export async function userExists(req, res, next) {
   const {name, email, password} = req.body;
+  // check if method is POST
   
+
   try {
     // check if user already exists with that email
     const {rows: users} = await db.query(
@@ -14,15 +16,8 @@ export async function userExists(req, res, next) {
     if (user) {
       return res.status(409).send("User with that email already exists.");
     }
-
-    // hash password
-    const hashedPassword = bcrypt.hashSync(password, 10);
-
-    res.locals.userSignUp = {
-      name,
-      email,
-      password: hashedPassword
-    };
+    
+   
     next();
     
   } catch (err) {
